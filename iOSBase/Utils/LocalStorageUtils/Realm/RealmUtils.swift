b79fd7ebd8ef368da02ac4902d4d074a4f99e267
NamPLT <namplt071099@gmail.com>
2023-11-01 09:35:46 +0700

    Refactor code

--- iOSBase/Utils/LocalStorageUtils/Realm/RealmUtils.swift
+++ iOSBase/Utils/LocalStorageUtils/Realm/RealmUtils.swift
@@ -13,44 +13,33 @@ final class RealmUtils: LocalStorage {
     let realm = try? Realm()
     
     func create(data: LocalStorageInputCreateData) throws {
-        switch data.self {
-        case .realm(let object):
+        if case let .realm(object) = data {
             try realm?.write({
                 realm?.add(object)
             })
-        default:
-            throw NSError()
         }
     }
     
     func read(data: LocalStorageInputReadData) throws -> Any? {
-        switch data.self {
-        case .realm(let type):
+        if case let .realm(type) = data {
             return realm?.objects(type)
-        default:
-            throw NSError()
         }
+        return nil
     }
     
     func update(data: LocalStorageInputUpdateData) throws {
-        switch data.self {
-        case .realm(let object):
+        if case let .realm(object) = data {
             try realm?.write({
                 realm?.add(object, update: .modified)
             })
-        default:
-            throw NSError()
         }
     }
     
     func delete(data: LocalStorageInputDeleteData) throws {
-        switch data.self {
-        case .realm(let object):
+        if case let .realm(object) = data {
             try realm?.write({
                 realm?.delete(object)
             })
-        default:
-            throw NSError()
         }
     }
 }
--- iOSBase/Utils/LocalStorageUtils/UserDefaults/UserDefaultsUtils.swift
+++ iOSBase/Utils/LocalStorageUtils/UserDefaults/UserDefaultsUtils.swift
@@ -12,42 +12,31 @@ final class UserDefaultsUtils: LocalStorage {
     let userDefaults = UserDefaults.standard
     
     func create(data: LocalStorageInputCreateData) throws {
-        switch data.self {
-        case .userDefaults(let dict):
+        if case let .userDefaults(dict) = data {
             for (key, value) in dict {
                 userDefaults.setValue(value, forKey: key)
             }
-        default:
-            throw NSError()
         }
     }
     
     func read(data: LocalStorageInputReadData) throws -> Any? {
-        switch data.self {
-        case .userDefaults(let key):
+        if case let .userDefaults(key) = data {
             return userDefaults.object(forKey: key)
-        default:
-            throw NSError()
         }
+        return nil
     }
     
     func update(data: LocalStorageInputUpdateData) throws {
-        switch data.self {
-        case .userDefaults(let dict):
+        if case let .userDefaults(dict) = data {
             for (key, value) in dict where userDefaults.object(forKey: key) != nil {
                 userDefaults.setValue(value, forKey: key)
             }
-        default:
-            throw NSError()
         }
     }
     
     func delete(data: LocalStorageInputDeleteData) throws {
-        switch data.self {
-        case .userDefaults(let key):
+        if case let .userDefaults(key) = data {
             userDefaults.removeObject(forKey: key)
-        default:
-            throw NSError()
         }
     }
 }
